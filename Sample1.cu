c8b81c246d07313d05c038c226bdfded67fd134a
Vrushank Ahire <146528953+Vrushank-Ahire@users.noreply.github.com>
2024-05-13 12:13:17 +0530

    Sample

---
+++ Sample1.cu
@@ -0,0 +1,21 @@
+int counter = 0;
+int incrementCounter();
+int main() {
+    int num = 10;
+    int total = 0;
+
+    char * hello;
+
+    while (i==2) {
+        total = i / 2;
+    }
+
+    printf("Total: ", total);
+
+    return 0;
+}
+
+int incrementCounter() {
+    counter = counter+1;
+    printf("Total: ", total);
+}
